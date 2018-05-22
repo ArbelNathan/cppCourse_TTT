cdc626ef54c49662aab92f47affc5958206b3749
Arbel nathan <arbel.nathan0@gmail.com>
2018-05-22 16:36:40 +0000

    champion done first try

--- Champion.cpp
+++ Champion.cpp
@@ -1,14 +1,55 @@
 #include "Champion.h"
 
 
+
+
 const Coordinate Champion::play(const Board& board) {
-	for (uint x=0; x<board.size(); ++x) {
-		for (uint y=0; y<board.size(); ++y) {
+	if(strcmp(rival,"XYPlayer")) return xyplayer(board);
+	if(strcmp(rival,"YXPlayer")) return yxplayer(board);
+	if(strcmp(rival,"IllegalPlayer")) return IllegalPlayer(board);
+	else return ExceptionPlayer(board);
+}
+
+private void Champion::getRival(string name, bool first){
+	this->rival=name;
+	this->meFirst=first;
+}
+
+private const Coordinate Champion::xyplayer(const Board& board){
+	if(meFirst){
+		YXPlayer player;
+		return player.play(board);
+	}
+	else{
+		uint y=1;
+		for (uint x=0; x<board.size(); ++x) {
 			Coordinate c{x,y};
 			if (board[c]=='.') {
 				return c;
 			}
 		}
 	}
-	return {0,0};  // did not find an empty square - play on the top-left
+}	
+private const Coordinate Champion::yxplayer(const Board& board){
+	if(meFirst){
+		XYPlayer player;
+		return player.play(board);
+	}
+	else{
+		uint x=1;
+		for (uint y=0; x<board.size(); ++x) {
+			Coordinate c{x,y};
+			if (board[c]=='.') {
+				return c;
+			}
+		}
+	}
+}	
+private const Coordinate Champion::IllegalPlayer(const Board& board){
+		XYPlayer player;
+		return player.play(board);	
 }
+private const Coordinate Champion::ExceptionPlayer(const Board& board){
+		XYPlayer player;
+		return player.play(board);
+} 
--- Champion.h
+++ Champion.h
@@ -8,4 +8,8 @@ class Champion: public Player{
 	public:
 		const string name() const override { return "Inna and Arbel"; }
 		const Coordinate play(const Board& board) override;
+		private const Coordinate xyplayer(const Board& board); 
+		private const Coordinate yxplayer(const Board& board);
+		private const Coordinate IllegalPlayer(const Board& board);
+		private const Coordinate ExceptionPlayer(const Board& board);
 };

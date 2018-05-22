c39d30304e5c9c513234ff7179c89b2c23d98118
innay96 <33022672+innay96@users.noreply.github.com>
2018-05-22 20:14:15 +0300

    Update Champion.h

--- Champion.h
+++ Champion.h
@@ -9,10 +9,9 @@ class Champion: public Player{
 	private:
 		string rival;
 		bool meFirst;
-		void getRival(string name, bool first);
 	
 	public:
 		const string name() const override { return "Inna , Arbel and Anna"; }
 		const Coordinate play(const Board& board) override;
-		
+		void getRival(string name, bool first);
 };

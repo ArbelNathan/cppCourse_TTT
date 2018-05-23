adc941e9be3897cd94249c089f4ff5e43bc7c674
annty2 <33619352+annty2@users.noreply.github.com>
2018-05-24 00:05:17 +0300

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -114,11 +114,11 @@ Player& TicTacToe::winner() const
 
 void TicTacToe::Rival(Player& xPlayer, Player& oPlayer){
     if (xPlayer.name()=="Inna , Arbel and Anna"){
-       Champion::xPlayer.getRival(oPlayer.name(), false);
+       Champion::((Champion)xPlayer).getRival(oPlayer.name(), false);
         
     }
     else if (oPlayer.name()=="Inna , Arbel and Anna"){
-        Champion::oPlayer.getRival(xPlayer.name(), false);
+        Champion::((Champion)oPlayer).getRival(xPlayer.name(), false);
     }
     
 }	

bff217409c97f03a2c36f74be63dc23717de8fef
annty2 <33619352+annty2@users.noreply.github.com>
2018-05-23 16:09:29 +0300

    Update Coordinate.h

--- Coordinate.h
+++ Coordinate.h
@@ -7,6 +7,7 @@ class Coordinate{
 	private:
 		int row,col;
 	public:
+		Coordinate(){row=0; col=0;}
         	Coordinate(int a, int b){ row = a; col = b; }
 		int getRow(){return row;}
 		int getCol(){return col;}

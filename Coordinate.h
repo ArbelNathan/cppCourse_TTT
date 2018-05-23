7c719a89b166f41cf673163c526c8591b9b53b4c
innay96 <33022672+innay96@users.noreply.github.com>
2018-05-23 20:48:29 +0300

    Update Coordinate.h

--- Coordinate.h
+++ Coordinate.h
@@ -4,11 +4,11 @@ using namespace std;
 
 class Coordinate{
 	private:
-		int row,col;
+		uint row,col;
 	public:
 		Coordinate(){row=0; col=0;}
-        	Coordinate(int a, int b){ row = a; col = b; }
-		int getRow(){return row;}
-		int getCol(){return col;}
+        	Coordinate(uint a, uint b){ row = a; col = b; }
+		uint getRow(){return row;}
+		uint getCol(){return col;}
 
 };

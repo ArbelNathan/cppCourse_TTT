22c8373de094fdc85cc4d42f92bd401b4c9acf41
ArbelNathan <33412258+ArbelNathan@users.noreply.github.com>
2018-05-23 15:44:35 +0300

    try

---
+++ Champion.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string>
+#include <string.h>
+#include "DummyPlayers.h"
+#include "Coordinate.h"
+using namespace std;
+#pragma once
+
+
+class Champion: public Player{
+	private:
+		string rival;
+		bool meFirst;
+	
+	public:
+		const string name() const override { return "Inna , Arbel and Anna"; }
+		const Coordinate play(const Board& board) override;
+		void getRival(string name, bool first);
+};
